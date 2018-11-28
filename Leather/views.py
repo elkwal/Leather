2bffab45ff0ba98de9b2859d0e381820e733a106
Gift-Lumumba <giftlumumba2@gmail.com>
2018-11-28 09:21:36 +0300

    created a registration form

--- Leather/views.py
+++ Leather/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404
+from .models import Category,Product
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+    current_user = request.user
+    # projects = Project.get_all()
+    return render(request,'index.html',{})
+
