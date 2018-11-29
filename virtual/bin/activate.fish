4018f53c952321eae242e3243b857de4dc368cce
Ekiru Timothy <ekiru@outlook.com>
2018-11-29 23:38:43 +0300

    created a virtual environment

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringaschool/Documents/Leather/virtual"
+set -gx VIRTUAL_ENV "/Users/ekiru/Documents/Sly/Leather/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
